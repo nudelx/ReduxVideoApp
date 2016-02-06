bf3cf2f23988db9295f4877b4f563c3222394342
Alex Nudelman <nudelx@gmail.com>
2016-02-06 23:22:06 +0200

    removed duplicate

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,17 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+
+  render() {
+    return <input onChange={this.onInputChange} />
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
+}
 
 
 export default SearchBar;
