2aa6f760572f6090c36203854e76e0e43fec77ea
Alex Nudelman <nudelx@gmail.com>
2016-02-07 00:05:39 +0200

    added new listener

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,12 +3,24 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+  constructor(props){
+
+    super(props);
+    this.state = {term: ''};
+  }
+
   render() {
-    return <input onChange={this.onInputChange} />
+    return (
+      <div>
+        <input onChange={ (event) => this.onInputChange(event) } />
+      </div>
+    )
   }
 
   onInputChange(event) {
-    console.log(event.target.value)
+      
+      this.setState({term: event.target.value})
+
   }
 
 }
