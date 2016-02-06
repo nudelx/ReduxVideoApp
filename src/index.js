84d9e825f04884d90fcf5912299e4388b14748b9
Alex Nudelman <nudelx@gmail.com>
2016-02-06 22:50:28 +0200

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 //Create New component
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyArFE0FKjiGoX-0wKSay_riAbAemd8t63U'
 /// const
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 
