6f2331c368560022a0bacea8989286507c64c0a8
Alex Nudelman <nudelx@gmail.com>
2016-02-06 23:02:04 +0200

    added ES6 class

---
+++ src/componets/search_bar.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+//some data 
+class SearchBar extends Component {
+  constructor() {
+
+  }
+
+  render() {
+    return <input />
+  }
+}
+
+
+export default SearchBar;
