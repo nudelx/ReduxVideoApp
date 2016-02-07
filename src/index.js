779efcb1d21dcf046ee9415d712eef3ccb127a24
Alex Nudelman <nudelx@gmail.com>
2016-02-07 21:43:51 +0200

    done with list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 //Create New component
-import React from 'react';
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import TYSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyArFE0FKjiGoX-0wKSay_riAbAemd8t63U';
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+
+class App extends Component {
+
+
+	constructor(props){
+		super(props);
+		this.state = {videos: [] };
+
+		TYSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({videos})
+		})
+
+	}
+
+
+	render(){
+		return (
+    	<div>
+      	<SearchBar />
+				<VideoList videos={this.state.videos}/>
+    	</div>
+  	);
+	}
 }
 
 
